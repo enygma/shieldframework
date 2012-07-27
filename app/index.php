85e83eb474fcc74fdbde8b1f9ff96b38ed45eb93
Devon Hazelett <mr.evoviii@yahoo.com>
2012-07-27 14:08:31 -0700

    clean up initial index demo

--- app/index.php
+++ app/index.php
@@ -3,18 +3,19 @@ include_once '../Shield/Shield.php';
 
 $app = new Shield\Shield();
 
-$app->get('/',function() use ($app){
+$app->get('/', function() use ($app){
 
-    $app->view->set('test','<a href="">foodles</a>');
-    $app->filter->add('test','email');
-
-    echo 'in: '.$app->input->get('test').'<br/>';
-    echo 'in1: '.$app->input->get('test1').'<br/>';
-
-    return $app->view->render('index1 [test]');
+    $app->view->set('test', 'foodles');
+    $app->filter->add('test', 'email');
+    
+    echo "?test : {$app->input->get('test')}\n";;
+    echo "&test1: {$app->input->get('test1')}\n";
+    
+    return $app->view->render('index1: <a href="#[test]">[fake]</a>');
 });
-$app->get('/test',function(){
+$app->get('/test', function(){
     echo 'test';
 });
-
+echo "<pre>";
 $app->run();
+echo "</pre>";
