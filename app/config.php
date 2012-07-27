deb2e4429458fec7fab0fb62b7c8e87110ae49c6
Chris Cornutt <ccornutt@isightpartners.com>
2012-07-27 09:55:32 -0500

    updating sesssion handling to use recommended MCRYPT_MODE_CBC, added config option to set your own cyrpt key

--- app/config.php
+++ app/config.php
@@ -5,4 +5,9 @@ return array(
      * You can use this setting to specify a log file directory
      */
     //'log_path' => '/tmp'
+    
+    /**
+     * Use this to set the cypher key for the session to your value
+     */
+    //'session_key' => null
 );
