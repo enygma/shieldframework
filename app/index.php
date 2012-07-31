61cf5e004f48ca2974d5f0a4eca3d7402a719a1e
Chris Cornutt <ccornutt@isightpartners.com>
2012-07-31 15:47:42 -0500

    updating the app/index.php example with comments about what's happening

--- app/index.php
+++ app/index.php
@@ -5,17 +5,30 @@ $app = new Shield\Shield();
 
 $app->get('/', function() use ($app){
 
+    /** Add some View values */
+    $app->view->set('fake','<b>link text</b>');
     $app->view->set('test', 'foodles');
+
+    /** Add a filter of type "email" */
     $app->filter->add('test', 'email');
-    
+
+    /** GETs from the URL, "?test=foo"
+        this will be empty because there's an "email" filter on it */
     echo "?test : {$app->input->get('test')}\n";;
+
+    /** GETs from the URL, "&test1=foo" */
     echo "&test1: {$app->input->get('test1')}\n";
     
+    /** Render the template output */
     return $app->view->render('index1: <a href="#[test]">[fake]</a>');
 });
+
+/** Define a "/test" route */
 $app->get('/test', function(){
-    echo 'test';
+    echo 'This is the Test route';
 });
+
+
 echo "<pre>";
 $app->run();
 echo "</pre>";
